cca18fa1709f8d99570a8f49cbcc0d01abccaf98
Francis-Gurr <francis.gurr@gmail.com>
2021-02-16 16:08:07 +0000

    added basic header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Map from './components/Map';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Map />
     </div>
   );
