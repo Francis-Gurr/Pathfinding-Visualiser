096331c45d983987dd77cbe45362254cde73eaad
Francis-Gurr <francis.gurr@gmail.com>
2021-02-18 21:33:02 +0000

    put all binary heap functions inside binaryHeap() function

--- server/algorithms/BinaryHeap.js
+++ server/algorithms/BinaryHeap.js
@@ -1,85 +1,87 @@
-const binHeap = [];
-const map = new Map(); // Index of each key
+const binaryHeap = () => {
+  let heap = [];
+  let map = new Map(); // Index of each key
 
-// Swap two keys in a heap
-const swap = (i, j) => {
-  const temp = binHeap[i];
-  binHeap[i] = binHeap[j];
-  binHeap[j] = temp;
-  // Update index of swapped keys in map
-  map[binHeap[i]] = i;
-  map[binHeap[j]] = j;
-  return;
-};
+  // Swap two keys in a heap
+  const swap = (i, j) => {
+    const temp = heap[i];
+    heap[i] = heap[j];
+    heap[j] = temp;
+    // Update index of swapped keys in map
+    map[heap[i]] = i;
+    map[heap[j]] = j;
+    return;
+  };
 
-// Bubble up a key
-const bubbleUp = (i) => {
-  while (i > 0) {
-    // Swap if value of key is smaller than value of parent
-    let parentIdx = Math.floor(i / 2);
-    if (binHeap[i] < binHeap[parentIdx]) {
-      swap(i, parentIdx);
-      i = parentIdx;
-    } else {
-      break;
+  // Bubble up a key
+  const bubbleUp = (i) => {
+    while (i > 0) {
+      // Swap if value of key is smaller than value of parent
+      let parentIdx = Math.floor(i / 2);
+      if (heap[i] < heap[parentIdx]) {
+        swap(i, parentIdx);
+        i = parentIdx;
+      } else {
+        break;
+      }
     }
-  }
-  return;
-};
+    return;
+  };
 
-// Insert a new key to the heap
-const insert = (key) => {
-  let i = binHeap.length; // Index at which to put new key (i.e at end of array)
-  binHeap.push(key);
-  map[key] = i;
-  bubbleUp(i);
-  return;
-};
+  // Insert a new key to the heap
+  const insert = (key) => {
+    let i = heap.length; // Index at which to put new key (i.e at end of array)
+    heap.push(key);
+    map[key] = i;
+    bubbleUp(i);
+    return;
+  };
 
-// Extract
-const extract = () => {
-  const root = binHeap[0];
-  map.delete[root]; // Remove key from map
-  // Put last key in the roots place
-  binHeap[0] = binHeap[binHeap.length - 1];
-  binHeap.pop();
-  map[binHeap[0]] = 0;
+  // Extract
+  const extract = () => {
+    const root = heap[0];
+    map.delete[root]; // Remove key from map
+    // Put last key in the roots place
+    heap[0] = heap[heap.length - 1];
+    heap.pop();
+    map[heap[0]] = 0;
 
-  // Bubble down the new root
-  let parentIdx = 0;
-  while (2 * parentIdx <= binHeap.length) {
-    // Get the child with the smallest value
-    let minChildIdx;
-    // If only one child, then it is the minimum
-    if (2 * parentIdx + 1 > binHeap.length) {
-      minChildIdx = 2 * parentIdx;
-    } else {
-      // Else compare the two children to find smallest
-      minChildIdx =
-        binHeap[2 * parentIdx] < binHeap[2 * parentIdx + 1]
-          ? 2 * parentIdx
-          : 2 * parentIdx + 1;
+    // Bubble down the new root
+    let parentIdx = 0;
+    while (2 * parentIdx <= heap.length) {
+      // Get the child with the smallest value
+      let minChildIdx;
+      // If only one child, then it is the minimum
+      // Else compare both children to find smallest
+      if (2 * parentIdx + 1 > heap.length) {
+        minChildIdx = 2 * parentIdx;
+      } else {
+        minChildIdx =
+          heap[2 * parentIdx] < heap[2 * parentIdx + 1]
+            ? 2 * parentIdx
+            : 2 * parentIdx + 1;
+      }
+
+      // If the parent value is greater than the child; swap them
+      if (heap[parentIdx] > heap[minChildIdx]) {
+        swap(parentIdx, minChildIdx);
+        parentIdx = minChildIdx;
+      } else {
+        break;
+      }
     }
-    // If the parent value is greater than the child; swap them
-    if (binHeap[parentIdx] > binHeap[minChildIdx]) {
-      swap(parentIdx, minChildIdx);
-      parentIdx = minChildIdx;
-    } else {
-      break;
-    }
-  }
-  return root;
-};
+    return root;
+  };
 
-// Decrease-Key
-const decreaseKey = (key, newValue) => {
-  let i = map(key);
-  //map.remove(key);
-  binHeap[i] = newValue;
-  //map[binHeap[i]] = i;
-  bubbleUp(i);
-  return;
+  // Decrease-Key
+  const decreaseKey = (key, newValue) => {
+    let i = map(key);
+    //map.remove(key);
+    heap[i] = newValue;
+    //map[binHeap[i]] = i;
+    bubbleUp(i);
+    return;
+  };
 };
-
 //module.exports = { func };
-export default { insert, extract, decreaseKey };
+export default { binaryHeap };
