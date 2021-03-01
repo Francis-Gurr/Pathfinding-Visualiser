0a566b19f79d0282e59b5df1c98707a82a52f7d0
Francis-Gurr <francis.gurr@gmail.com>
2021-03-01 18:45:19 +0000

    added query functions for edges table

--- server/algorithms/Dijkstra.js
+++ server/algorithms/Dijkstra.js
@@ -2,17 +2,64 @@
 import heap from "./BinaryHeap";
 import PathNode from "./PathNode";
 
-const Dikstra = (src, target, mode) => {
+const Dikstra = (src, target, mode, con) => {
   const src = this.src;
   const target = this.target;
-  const mode = this.mode; // Mode of transport (0: car, 1: cycle, 2: foot)
+  const mode = this.mode; // Mode of transport (0: car, 1: bike, 2: foot)
   const binHeap = new BinaryHeap();
   let visitedNodes = []; // Array of nodes in visited order
 
-  // Return rows where nodeId is in target column
-  const queryTargets = (nodeId) => {};
-  // Return rows where nodeId is in source column (i.e. reverse direction)
-  const querySources = (nodeId) => {};
+  // Get edges in the forward direction (i.e. nodeId is the target)
+  const queryForward = (nodeId) => {
+    let sqlQuery = "SELECT * FROM edges WHERE target = ? AND ";
+    switch (mode) {
+      //car
+      case 0:
+        sqlQuery + mysql.escape("car != 0");
+        break;
+      // bike
+      case 1:
+        sqlQuery + mysql.escape("bike != 0");
+        break;
+      // foot
+      case 2:
+        sqlQuery + mysql.escape("foot != 0");
+        break;
+    }
+    con.query(sqlQuery, [nodeId], (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        return results;
+      }
+    });
+  };
+
+  // Get edges in the opposite direction (i.e. nodeId is the source)
+  const queryReverse = (nodeId) => {
+    let sqlQuery = "SELECT * FROM edges WHERE source = ? AND ";
+    switch (mode) {
+      //car
+      case 0:
+        sqlQuery + mysql.escape("car_reverse != 0");
+        break;
+      // bike
+      case 1:
+        sqlQuery + mysql.escape("bike_reverse != 0");
+        break;
+      // foot
+      case 2:
+        sqlQuery + mysql.escape("foot != 0");
+        break;
+    }
+    con.query(sqlQuery, [nodeId], (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        return results;
+      }
+    });
+  };
 
   // Get the neighbours of a node
   const getNeighbours = (node) => {
